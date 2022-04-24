64e57bb373d99a0557357911927251848335b7f5
Dedi Setiawan <dediwsn@gmail.com>
2022-04-24 05:05:16 +0000

    update dummy end point for chart data

--- packages/raise/src/vendor/vendor.controller.ts
+++ packages/raise/src/vendor/vendor.controller.ts
@@ -17,10 +17,46 @@ export class VendorController {
   }
 
   @Get(':vendorId/getChartData')
-  async getChartData() {
-    return {
-      a: '100',
-      b: '200',
-    };
+  async getChartData(){
+    const Income =
+    [
+      {
+        name: 'Monthly',
+        value: '4,900.50 SAR',
+        data: [{ name: 'Monthly', data: [10, 41, 35, 51, 49, 62, 69, 91, 148] }],
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      },
+      {
+        name: "Daily (last 7 days)",
+        value: '1,900.50 SAR', //sum amount of last 7 days
+        chartData: [{ name: 'Income1', data: [10, 41, 35, 51, 49, 62, 69, 91, 48]},
+                    { name: 'Income2', data: [10, 41, 35, 51, 49, 62, 69, 91, 48] }
+                  ],
+        categories: ['1st', '2nd', '3rd', '4th', '5th', '6th ', '7th'],
+      },
+    ];
+
+    const Campaign =
+    [
+      {
+        name: 'Monthly',
+        value: '$4,900.50',
+        data: [{ name: 'Monthly', data: [10, 41, 35, 51, 49, 62, 69, 91, 148] }],
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      },
+      {
+        name: "Daily (last 7 days)",
+        value: '2,500.00 SAR',
+        chartData: [{ name: 'Campaign1', data: [10, 41, 35, 51, 49, 62, 69, 91, 48] },
+                    { name: 'Campaign2', data: [15, 21, 65, 11, 44, 42, 59, 71, 18] }
+                  ],
+                  categories: ['1st', '2nd', '3rd', '4th', '5th', '6th ', '7th'],
+      },
+    ];
+
+    return ({
+      Income,
+      Campaign
+    })
   }
 }
