75af2e6319d2fb98ebe536d104e356b710c1ffcd
simeon.nugraha1 <simeon.nugraha@dkatalis.com>
2022-04-24 20:14:29 +0700

    chore: add swagger module

--- packages/raise/src/main.ts
+++ packages/raise/src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+  const config = app.get<ConfigService>(ConfigService);
+  console.log(config.get<string>('APP_ENV'));
+
+  if (config.get<string>('APP_ENV') === 'dev') {
+    const config = new DocumentBuilder()
+      .setTitle('TMRA Raise')
+      .setDescription('TMRA Raise API')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
