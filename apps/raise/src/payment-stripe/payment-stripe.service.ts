061c1b24c07287b9734f0dd76ba99f0c5a4ea4b8
Fatimah Nurul Insani <fatimahnurulinsani@gmail.com>
2022-07-21 16:09:53 +0000

    feat: implement trace for stripe request

--- apps/raise/src/payment-stripe/payment-stripe.service.ts
+++ apps/raise/src/payment-stripe/payment-stripe.service.ts
@@ -24,6 +24,7 @@ import {
   AnonymousDocument,
 } from 'src/donor/schema/anonymous.schema';
 import { User, UserDocument } from 'src/user/schema/user.schema';
+import { SpanStatusCode, trace } from '@opentelemetry/api';
 
 @Injectable()
 export class PaymentStripeService {
@@ -49,305 +50,325 @@ export class PaymentStripeService {
   ) {}
 
   async stripeRequest(payment: PaymentRequestDto) {
-    this.logger.debug('stripeRequest...');
-    let txtMessage = '';
-    let stripeCallbackUrl = '';
-    // let amount = '';
-    // let donorId = '';
-    let isAnonymous = false;
-    let donor = null;
-    // let donorName = '';
-    let currency = payment.currency;
-
-    const ObjectId = require('mongoose').Types.ObjectId;
-    console.log(payment);
-    if (
-      !payment.organizationId ||
-      !payment.campaignId ||
-      !payment.donorId ||
-      !ObjectId.isValid(payment.campaignId) ||
-      !ObjectId.isValid(payment.organizationId) ||
-      !payment.success_url ||
-      !payment.cancel_url ||
-      !payment.price ||
-      !payment.quantity
-    ) {
-      txtMessage = 'Bad Request';
-      return {
-        statusCode: 400,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          message: txtMessage,
-        }),
-      };
-    }
-
-    if (!['GBP', 'SAR'].includes(currency)) {
-      txtMessage = 'Bad Request';
-      return {
-        statusCode: 400,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          message: 'Currency must be GBP or SAR',
-        }),
-      };
-    }
-
-    const getOrganization = await this.organizationModel.findOne({
-      _id: payment.organizationId,
+    const tracer = trace.getTracer('tmra-raise');
+    const span = tracer.startSpan('stripe-request', {
+      attributes: { 'donor.firstName': '-' },
     });
-    console.log('currency', getOrganization?.defaultCurrency);
-    if (!getOrganization) {
-      txtMessage = `request rejected organizationId not found`;
-      return {
-        statusCode: 514,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          message: txtMessage,
-        }),
-      };
-    } else if (!currency) {
-      currency = getOrganization['defaultCurrency'];
-    }
-    console.log('debug', payment.campaignId);
-    // if (payment.campaignId) {
-    const getCampaign = await this.campaignModel
-      .findOne({
-        _id: payment.campaignId,
-      })
-      .exec();
+    try {
+      // Do some work here
 
-    console.log('debug', getCampaign);
-    let dataAmount = parseFloat(payment.quantity); // let's assume it will be multiple by 1 (price)
-    if (!getCampaign) {
-      txtMessage = `request rejected campaignId not found`;
-      return {
-        statusCode: 514,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          message: txtMessage,
-        }),
-      };
-    } else if (
-      parseFloat(getCampaign.amountProgress.toString()) + dataAmount >
-      parseFloat(getCampaign.amountTarget.toString())
-    ) {
-      return {
-        statusCode: 400,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          message: `Ammount is larger than the limit of the target ${getCampaign.amountTarget}`,
-        }),
-      };
-    }
-    // }
+      this.logger.debug('stripeRequest...');
+      let txtMessage = '';
+      let stripeCallbackUrl = '';
+      // let amount = '';
+      // let donorId = '';
+      let isAnonymous = false;
+      let donor = null;
+      // let donorName = '';
+      let currency = payment.currency;
 
-    console.log('debug', payment.organizationId);
-    const getSecretKey = await this.paymentGatewayModel.findOne(
-      { organizationId: ObjectId(payment.organizationId) },
-      { apiKey: 1, _id: 0 },
-    );
+      const ObjectId = require('mongoose').Types.ObjectId;
+      console.log(payment);
+      if (
+        !payment.organizationId ||
+        !payment.campaignId ||
+        !payment.donorId ||
+        !ObjectId.isValid(payment.campaignId) ||
+        !ObjectId.isValid(payment.organizationId) ||
+        !payment.success_url ||
+        !payment.cancel_url ||
+        !payment.price ||
+        !payment.quantity
+      ) {
+        txtMessage = 'Bad Request';
+        return {
+          statusCode: 400,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({
+            message: txtMessage,
+          }),
+        };
+      }
 
-    if (!getSecretKey) {
-      txtMessage = 'organization can not use Stripe Payment Gateway';
-      console.log('here');
-      return {
-        statusCode: 404, //resource not found
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          message: txtMessage,
-        }),
-      };
-    }
+      if (!['GBP', 'SAR'].includes(currency)) {
+        txtMessage = 'Bad Request';
+        return {
+          statusCode: 400,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({
+            message: 'Currency must be GBP or SAR',
+          }),
+        };
+      }
 
-    console.log(getSecretKey['apiKey']);
-    //let getUser = new UserModel();
-    console.log(payment.donorId);
-    if (payment.donorId) {
-      // console.log('donorid');
-      // console.log(payment.donorId);
-      if (!ObjectId.isValid(payment.donorId)) {
-        donor = await this.userModel.findOne({
-          _id: payment.donorId,
-        });
-        // if (donor) donorName = `${donor.firstname} ${donor.lastname ?? ''}`;
-      } else {
-        donor = await this.donorModel.findOne({
-          _id: payment.donorId,
-        });
+      const getOrganization = await this.organizationModel.findOne({
+        _id: payment.organizationId,
+      });
+      console.log('currency', getOrganization?.defaultCurrency);
+      if (!getOrganization) {
+        txtMessage = `request rejected organizationId not found`;
+        return {
+          statusCode: 514,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({
+            message: txtMessage,
+          }),
+        };
+      } else if (!currency) {
+        currency = getOrganization['defaultCurrency'];
+      }
+      console.log('debug', payment.campaignId);
+      // if (payment.campaignId) {
+      const getCampaign = await this.campaignModel
+        .findOne({
+          _id: payment.campaignId,
+        })
+        .exec();
 
-        if (!donor) {
-          donor = await this.anonymousModel.findOne({
+      console.log('debug', getCampaign);
+      let dataAmount = parseFloat(payment.quantity); // let's assume it will be multiple by 1 (price)
+      if (!getCampaign) {
+        txtMessage = `request rejected campaignId not found`;
+        return {
+          statusCode: 514,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({
+            message: txtMessage,
+          }),
+        };
+      } else if (
+        parseFloat(getCampaign.amountProgress.toString()) + dataAmount >
+        parseFloat(getCampaign.amountTarget.toString())
+      ) {
+        return {
+          statusCode: 400,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({
+            message: `Ammount is larger than the limit of the target ${getCampaign.amountTarget}`,
+          }),
+        };
+      }
+      // }
+
+      console.log('debug', payment.organizationId);
+      const getSecretKey = await this.paymentGatewayModel.findOne(
+        { organizationId: ObjectId(payment.organizationId) },
+        { apiKey: 1, _id: 0 },
+      );
+
+      if (!getSecretKey) {
+        txtMessage = 'organization can not use Stripe Payment Gateway';
+        console.log('here');
+        return {
+          statusCode: 404, //resource not found
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({
+            message: txtMessage,
+          }),
+        };
+      }
+
+      console.log(getSecretKey['apiKey']);
+      //let getUser = new UserModel();
+      console.log(payment.donorId);
+      if (payment.donorId) {
+        // console.log('donorid');
+        // console.log(payment.donorId);
+        if (!ObjectId.isValid(payment.donorId)) {
+          donor = await this.userModel.findOne({
             _id: payment.donorId,
           });
+          // if (donor) donorName = `${donor.firstname} ${donor.lastname ?? ''}`;
+        } else {
+          donor = await this.donorModel.findOne({
+            _id: payment.donorId,
+          });
+
           if (!donor) {
-            txtMessage = 'user not found,  donation service is not available';
-            return {
-              statusCode: 516, //user not found
-              headers: {
-                'Access-Control-Allow-Origin': '*',
-              },
-              body: JSON.stringify({
-                message: txtMessage,
-              }),
-            };
-          } else {
-            isAnonymous = true;
+            donor = await this.anonymousModel.findOne({
+              _id: payment.donorId,
+            });
+            if (!donor) {
+              txtMessage = 'user not found,  donation service is not available';
+              return {
+                statusCode: 516, //user not found
+                headers: {
+                  'Access-Control-Allow-Origin': '*',
+                },
+                body: JSON.stringify({
+                  message: txtMessage,
+                }),
+              };
+            } else {
+              isAnonymous = true;
+            }
           }
+
+          span.setAttribute('donor.firstName', donor?.firstName ?? '');
+
+          // if (donor) donorName = `${donor.firstName} ${donor.lastName ?? ''}`;
         }
-
-        // if (donor) donorName = `${donor.firstName} ${donor.lastName ?? ''}`;
       }
-    }
 
-    // const paymentJson = JSON.parse(JSON.stringify(payment));
-    const params = new URLSearchParams();
-    if (donor) {
-      params.append('customer_email', donor.email);
-      // params.append('customer', {'email': donor.email, name: donorName});
-      // params.append('customer_name', donorName);
-    }
-    params.append('success_url', payment.success_url);
-    params.append('cancel_url', payment.cancel_url);
-    params.append('line_items[0][price]', payment.price);
-    params.append('line_items[0][quantity]', payment.quantity);
-    params.append('mode', 'payment');
-    params.append('submit_type', 'donate'); //will enable button with label "donate"
-    console.log(params);
-    const options: AxiosRequestConfig<any> = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: 'Bearer ' + getSecretKey['apiKey'] + '',
-      },
-      params,
-      url: 'https://api.stripe.com/v1/checkout/sessions',
-    };
+      // const paymentJson = JSON.parse(JSON.stringify(payment));
+      const params = new URLSearchParams();
+      if (donor) {
+        params.append('customer_email', donor.email);
+        // params.append('customer', {'email': donor.email, name: donorName});
+        // params.append('customer_name', donorName);
+      }
+      params.append('success_url', payment.success_url);
+      params.append('cancel_url', payment.cancel_url);
+      params.append('line_items[0][price]', payment.price);
+      params.append('line_items[0][quantity]', payment.quantity);
+      params.append('mode', 'payment');
+      params.append('submit_type', 'donate'); //will enable button with label "donate"
+      console.log(params);
+      const options: AxiosRequestConfig<any> = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'Bearer ' + getSecretKey['apiKey'] + '',
+        },
+        params,
+        url: 'https://api.stripe.com/v1/checkout/sessions',
+      };
 
-    const data = await axios(options);
-    console.log(data);
-    if (!data) {
+      const data = await axios(options);
+      console.log(data);
+      if (!data) {
+        return {
+          statusCode: 504,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({
+            message: 'Gateway Timeout or Stripe API down',
+          }),
+        };
+      }
+
+      const amountStr = data['data']['amount_total'].toString();
+      const amount = amountStr.substring(0, amountStr.length - 2);
+
+      console.log('amount unit', amount);
+
+      //insert data to donation_log
+      let objectIdDonation = new ObjectId();
+      let now: Date = new Date();
+      const getDonationLog = await new this.donationLogModel({
+        _id: objectIdDonation,
+        nonprofitRealmId: ObjectId(payment.organizationId),
+        donorUserId: isAnonymous ? '' : payment.donorId,
+        // donorName: donor ? `${donor.firstName} ${donor.lastName}` : null,
+        // amount: payment.amount,
+        amount: Number(amount),
+        createdAt: now,
+        updatedAt: now,
+        campaignId: ObjectId(payment.campaignId),
+        currency: currency,
+        donationStatus: 'PENDING',
+        // type: payment.type,
+        // ipAddress: '',
+        // paymentGatewayId: 'PAYSTRIPE',
+        // transactionId: data['data']['id'],
+      }).save();
+
+      if (!getDonationLog) {
+        txtMessage = 'donation failed to save in mongodb';
+        return {
+          statusCode: 504,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({
+            message: txtMessage,
+          }),
+        };
+      }
+
+      //insert data to paymentData
+      let objectIdPayment = new ObjectId();
+      // this.logger.debug(isAnonymous.toString);
+      if (isAnonymous) {
+        // donor.donorLogId = objectIdDonation;
+        // donor.save();
+        await this.anonymousModel.findOneAndUpdate(
+          { _id: payment.donorId },
+          { donationLogId: objectIdDonation },
+        );
+      }
+      const insertPaymentData = await new this.paymentDataModel({
+        _id: objectIdPayment,
+        donationId: objectIdDonation,
+        merchantId: '',
+        payerId: '',
+        orderId: data.data['payment_intent'], //payment_intent ID
+        cardType: '',
+        cardScheme: '',
+        paymentDescription: '',
+        expiryMonth: '',
+        expiryYear: '',
+        responseStatus: '',
+        responseCode: '',
+        responseMessage: '',
+        cvvResult: '',
+        avsResult: '',
+        transactionTime: '',
+        paymentStatus: 'OPEN',
+      }).save();
+
+      console.log(insertPaymentData);
+
+      if (!insertPaymentData) {
+        txtMessage = 'payment data failed to save in mongodb';
+        return {
+          statusCode: 504,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({
+            message: txtMessage,
+          }),
+        };
+      }
+
+      console.log('debug', data);
+      console.log('debug', data['data']['id']);
+      stripeCallbackUrl = data['data']['url'];
+      txtMessage = `stripe request has been sent`;
+
       return {
-        statusCode: 504,
+        statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*',
+          // "Access-Control-Allow-Credentials": true,
         },
-        body: JSON.stringify({
-          message: 'Gateway Timeout or Stripe API down',
-        }),
+        stripeResponse: data['data'],
+        message: txtMessage,
       };
+    } catch (err) {
+      // When we catch an error, we want to show that an error occurred
+      span.setStatus({
+        code: SpanStatusCode.ERROR,
+        message: err.message,
+      });
+      throw err;
+    } finally {
+      // Every span must be ended or it will not be exported
+      span.end();
     }
-
-    const amountStr = data['data']['amount_total'].toString();
-    const amount = amountStr.substring(0, amountStr.length - 2);
-
-    console.log('amount unit', amount);
-
-    //insert data to donation_log
-    let objectIdDonation = new ObjectId();
-    let now: Date = new Date();
-    const getDonationLog = await new this.donationLogModel({
-      _id: objectIdDonation,
-      nonprofitRealmId: ObjectId(payment.organizationId),
-      donorUserId: isAnonymous ? '' : payment.donorId,
-      // donorName: donor ? `${donor.firstName} ${donor.lastName}` : null,
-      // amount: payment.amount,
-      amount: Number(amount),
-      createdAt: now.toISOString(),
-      updatedAt: now.toISOString(),
-      campaignId: ObjectId(payment.campaignId),
-      currency: currency,
-      donationStatus: 'PENDING',
-      // type: payment.type,
-      // ipAddress: '',
-      // paymentGatewayId: 'PAYSTRIPE',
-      // transactionId: data['data']['id'],
-    }).save();
-
-    if (!getDonationLog) {
-      txtMessage = 'donation failed to save in mongodb';
-      return {
-        statusCode: 504,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          message: txtMessage,
-        }),
-      };
-    }
-
-    //insert data to paymentData
-    let objectIdPayment = new ObjectId();
-    // this.logger.debug(isAnonymous.toString);
-    if (isAnonymous) {
-      // donor.donorLogId = objectIdDonation;
-      // donor.save();
-      await this.anonymousModel.findOneAndUpdate(
-        { _id: payment.donorId },
-        { donationLogId: objectIdDonation },
-      );
-    }
-    const insertPaymentData = await new this.paymentDataModel({
-      _id: objectIdPayment,
-      donationId: objectIdDonation,
-      merchantId: '',
-      payerId: '',
-      orderId: data.data['payment_intent'], //payment_intent ID
-      cardType: '',
-      cardScheme: '',
-      paymentDescription: '',
-      expiryMonth: '',
-      expiryYear: '',
-      responseStatus: '',
-      responseCode: '',
-      responseMessage: '',
-      cvvResult: '',
-      avsResult: '',
-      transactionTime: '',
-      paymentStatus: 'OPEN',
-    }).save();
-
-    console.log(insertPaymentData);
-
-    if (!insertPaymentData) {
-      txtMessage = 'payment data failed to save in mongodb';
-      return {
-        statusCode: 504,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          message: txtMessage,
-        }),
-      };
-    }
-
-    console.log('debug', data);
-    console.log('debug', data['data']['id']);
-    stripeCallbackUrl = data['data']['url'];
-    txtMessage = `stripe request has been sent`;
-
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        // "Access-Control-Allow-Credentials": true,
-      },
-      stripeResponse: data['data'],
-      message: txtMessage,
-    };
   }
 }
