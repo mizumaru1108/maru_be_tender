fbe664065119f40355250e8a72beb223a7e4298c
nzf210 <62851@syahril.com>
2022-08-08 22:44:14 +0800

    update updateAt widget Services Cart Basket

--- apps/raise/src/widgets/dto/basket.dto.ts
+++ apps/raise/src/widgets/dto/basket.dto.ts
@@ -10,5 +10,19 @@ export class BasketDto {
   readonly unit: number;
   readonly isDeleted: boolean;
   readonly createdAt: string;
-  readonly updatedAt: string;
+  updatedAt: string;
+}
+
+export class BasketProjectDto {
+  // readonly id: string;
+  // readonly campaignId: Types.ObjectId;
+  // readonly donorId: Types.ObjectId;
+  // readonly donationType: string;
+  // readonly projectType: string;
+  // readonly currency: string;
+  // readonly amount: number;
+  // readonly unit: number;
+  // readonly isDeleted: boolean;
+  // readonly createdAt: string;
+  // readonly updatedAt: string;
 }
--- apps/raise/src/widgets/widgets.controller.ts
+++ apps/raise/src/widgets/widgets.controller.ts
@@ -8,14 +8,14 @@ import {
   Param,
 } from '@nestjs/common';
 import { rootLogger } from '../logger';
-import { BasketDto } from './dto/basket.dto';
+import { BasketProjectDto, BasketDto } from './dto';
 import { WidgetsService } from './widgets.service';
 
 @Controller('widgets')
 export class WidgetsController {
   private logger = rootLogger.child({ logger: WidgetsController.name });
 
-  constructor(private widgetsService: WidgetsService) {}
+  constructor(private widgetsService: WidgetsService) { }
 
   @Get('basket/list')
   async getBasketList(@Query('donorId') donorId: string) {
@@ -36,4 +36,25 @@ export class WidgetsController {
     this.logger.debug('update basket...');
     return await this.widgetsService.updateBasket(basketId, basketDto);
   }
+
+  // @Post('basketProject/create')
+  // async createBasketProject(@Body() basketProjectDto: BasketProjectDto) {
+  //   return await this.widgetsService.createProjectBasket(basketProjectDto);
+  // }
+
+  // @Patch('basketProject/:basketId')
+  // async updateProjectBasket(
+  //   @Param('basketId') basketId: string,
+  //   @Body() basketProjectDto: BasketProjectDto,
+  // ) {
+  //   this.logger.debug('update basket...');
+  //   return await this.widgetsService.updateProjectBasket(basketId, basketProjectDto);
+  // }
+
+  // @Get('basketProject/list')
+  // async getBasketProjectList(@Query('donorId') donorId: string) {
+  //   this.logger.debug('fetching basket list...');
+  //   return await this.widgetsService.getBasketProjectList(donorId);
+  // }
+
 }
