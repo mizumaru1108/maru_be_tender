1eb762dba1f837963aafe13cf32d14b6e54c4c33
rdanang-dev <rdanang.dev@gmail.com>
2022-09-16 19:45:08 +0700

    fix: dto wrong extends

--- apps/raise/src/campaign/dto/campaign-update.dto.ts
+++ apps/raise/src/campaign/dto/campaign-update.dto.ts
@@ -2,9 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsArray, IsNotEmpty, ValidateNested } from 'class-validator';
 import { UpdateImagePayload } from '../../commons/dtos/update-image-payload.dto';
-import { CampaignCreateDto } from './campaign-create.dto';
+import { CampaignCreateEditBaseDto } from './campaign-create-edit-base.dto';
 
-export class CampaignUpdateDto extends CampaignCreateDto {
+export class CampaignUpdateDto extends CampaignCreateEditBaseDto {
   @ApiProperty()
   @IsArray()
   @IsNotEmpty()
