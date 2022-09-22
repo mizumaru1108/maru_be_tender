4bd5f408976de5e1fa07e4559858bf48648d2adf
rdanang-dev <rdanang.dev@gmail.com>
2022-09-23 05:16:01 +0700

    feat: add prisma logger

--- apps/raise/src/prisma/prisma.service.ts
+++ apps/raise/src/prisma/prisma.service.ts
@@ -2,11 +2,20 @@ import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
-
-
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private configService: ConfigService) {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+      datasources: {
+        db: {
+          url: configService.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
   async onModuleInit() {
+    console.log('DATABASE_URL', this.configService.get('DATABASE_URL')!);
     await this.$connect();
   }
 
