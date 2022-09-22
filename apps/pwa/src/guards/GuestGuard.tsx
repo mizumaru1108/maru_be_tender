8b51868c7d29ddbebd79db8dac6c66aa0eb81d25
unknown <essamkayall1996@gmail.com>
2022-09-22 11:14:55 +0300

    [Fix] taking the user to his dashboard after logging to tender

--- apps/pwa/src/guards/GuestGuard.tsx
+++ apps/pwa/src/guards/GuestGuard.tsx
@@ -6,9 +6,23 @@ import useAuth from '../hooks/useAuth';
 // import { PATH_DASHBOARD } from '../routes/paths';
 // components
 import LoadingScreen from '../components/LoadingScreen';
+import { Role } from './RoleBasedGuard';
 
 // ----------------------------------------------------------------------
 
+const role_url_map = {
+  cluster_admin: '',
+  tender_accounts_manager: 'accounts-manager',
+  tender_admin: '',
+  tender_ceo: '',
+  tender_cashier: '',
+  tender_client: 'client',
+  tender_consultant: '',
+  tender_finance: '',
+  tender_moderator: 'moderator',
+  tender_project_manager: '',
+  tender_project_supervisor: '',
+};
 type GuestGuardProps = {
   children: ReactNode;
 };
@@ -16,11 +30,11 @@ type GuestGuardProps = {
 export default function GuestGuard({ children }: GuestGuardProps) {
   const { isAuthenticated, isInitialized, user } = useAuth();
 
-  console.log(user);
+  const role = user?.registrations[0].roles[0] as Role;
   // TODO map all the roles with their dashboard page
   // FOR testing now I will only redirect to the client dashboard page
   if (isAuthenticated) {
-    return <Navigate to={'/client/dashboard/app'} />;
+    return <Navigate to={`/${role_url_map[`${role}`]}/dashboard/app`} />;
   }
 
   if (!isInitialized) {
