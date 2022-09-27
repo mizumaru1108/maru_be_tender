daecd4a255367e845cc610aac8391a5c90ce6287
mizumaru1108 <haitsam.umar@gmail.com>
2022-09-27 14:03:30 +0700

    WIP message moderator

--- apps/pwa/src/pages/moderator/mock-data.ts
+++ apps/pwa/src/pages/moderator/mock-data.ts
@@ -118,7 +118,7 @@ export const CardTablePreviousSupportRequests = [
   },
 ] as ProjectCardProps[];
 
-export const Messages = [
+export const MessagesInternalCorespondence = [
   {
     partnerName: 'project1',
     projectName: 'name1',
@@ -132,5 +132,36 @@ export const Messages = [
     message:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem sequi, blanditiis provident assumenda ipsam officia ipsa repellendus quia quaerat reprehenderit inventore totam, necessitatibus ipsum ullam amet atque rerumiure saepe.',
     footer: new Date(2022, 8, 2, 15, 58),
+  },
+];
+export const MessagesExternalCorespondence = [
+  {
+    partnerName: 'project3',
+    projectName: 'name3',
+    message:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem sequi, blanditiis provident assumenda ipsam officia ipsa repellendus quia quaerat reprehenderit inventore totam, necessitatibus ipsum ullam amet atque rerumiure saepe.',
+    footer: new Date(2022, 8, 2, 15, 58),
+  },
+  {
+    partnerName: 'project4',
+    projectName: 'name4',
+    message:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem sequi, blanditiis provident assumenda ipsam officia ipsa repellendus quia quaerat reprehenderit inventore totam, necessitatibus ipsum ullam amet atque rerumiure saepe.',
+    footer: new Date(2022, 8, 2, 15, 58),
+  },
+];
+
+export const Message = [
+  {
+    sender: 'partner1',
+    dateCreated: String(new Date(2022, 8, 2, 15, 58)),
+    messageBody: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+    timeCreated: new Date().getHours() + ':' + new Date().getMinutes(),
+  },
+  {
+    sender: 'moderator',
+    dateCreated: String(new Date(2022, 8, 2, 15, 58)),
+    messageBody: 'Lorem ipsum dolor sit amet consectetur adipisicing elit ...',
+    timeCreated: new Date().getHours() + ':' + new Date().getMinutes(),
   },
 ];
