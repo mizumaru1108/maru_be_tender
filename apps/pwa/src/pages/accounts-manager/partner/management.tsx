2442ed39d699d010013804f019378a509a3b6685
Yayan Handani <others.handani@gmail.com>
2022-10-01 16:56:21 +0700

    fix: partner management pages

--- apps/pwa/src/pages/accounts-manager/partner/management.tsx
+++ apps/pwa/src/pages/accounts-manager/partner/management.tsx
@@ -1,18 +1,40 @@
 import { useEffect, useState } from 'react';
 // material
-import { Container, styled, Skeleton } from '@mui/material';
+import { Container, styled, Skeleton, Grid, Button } from '@mui/material';
 // components
 import Page from 'components/Page';
 import { TableAMCustom } from 'components/table';
-// mock
-// import { AM_PARTNER_MANAGEMENT } from '../mock-data';
 // hooks
-import axios from 'axios';
-import { HASURA_GRAPHQL_URL } from 'config';
+import { useQuery } from 'urql';
+import useAuth from 'hooks/useAuth';
+import { getAllClient } from 'queries/account_manager/partnerManagement';
 //
 import { IPropsTablesList } from 'components/table/type';
 
 // -------------------------------------------------------------------------------
+
+const CLIENT_STATUS = [
+  {
+    label: 'partner_management.button.all_partners',
+    value: 'ALL_CLIENT',
+  },
+  {
+    label: 'partner_management.button.new_partners',
+    value: 'WAITING_FOR_ACTIVATION',
+  },
+  {
+    label: 'partner_management.button.active_partners',
+    value: 'ACTIVE_ACCOUNT',
+  },
+  {
+    label: 'partner_management.button.rejected_partners',
+    value: 'REVISED_ACCOUNT',
+  },
+  {
+    label: 'partner_management.button.suspended_partners',
+    value: 'SUSPENDED_ACCOUNT',
+  },
+];
 
 const ContentStyle = styled('div')(({ theme }) => ({
   maxWidth: '100%',
@@ -23,76 +45,68 @@ const ContentStyle = styled('div')(({ theme }) => ({
   rowGap: '50px',
 }));
 
+// -------------------------------------------------------------------------------
+
 function PartnerManagementPage() {
-  const path = HASURA_GRAPHQL_URL;
-  const secret = 'hbd4KbAS5XjHw5';
+  const { user } = useAuth();
+  const [clientData, setClientData] = useState<IPropsTablesList[] | null>(null);
+  const [activeButton, setActiveButton] = useState<string>('ALL_CLIENT');
 
-  const [newJoinRequestData, setNewJoinRequestData] = useState<IPropsTablesList[] | null>(null);
+  // Table Client
+  const [resultAllClient, reexecuteAllClient] = useQuery({
+    query: getAllClient,
+  });
 
-  const getNewJoinRequestData = async () => {
-    const queryData = {
-      query: `
-        query MyQuery {
-          client_data {
-            created_at
-            email
-            id
-            ceo_mobile
-            ceo_name
-          }
-        }
-      `,
-    };
-
-    const headers = {
-      headers: {
-        'Content-Type': 'aplication/json',
-        'x-hasura-admin-secret': `${secret}`,
-      },
-    };
-
-    await axios
-      .post(`${path}`, queryData, headers)
-      .then(({ data }) => {
-        if (data.data) {
-          const clientData = data?.data?.client_data.map((v: any) => ({
-            id: v.id,
-            partner_name: v.ceo_name,
-            createdAt: v.created_at,
-            account_status: 'waiting',
-            events: v.email,
-          }));
-
-          setNewJoinRequestData(clientData);
-          console.log('errorResponse', data.errors);
-        }
-      })
-      .catch((err) => console.log('error', err.message));
-  };
+  const {
+    data: resultAllClientRequest,
+    fetching: fetchingAllClientRequest,
+    error: errorAllClientRequest,
+  } = resultAllClient;
 
   useEffect(() => {
-    getNewJoinRequestData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (!fetchingAllClientRequest && resultAllClientRequest) {
+      const resultAllClientData = resultAllClientRequest?.client_data.map((v: any) => ({
+        id: v.id,
+        partner_name: v.entity,
+        createdAt: v.created_at,
+        account_status: v.status,
+        events: v.id,
+      }));
+
+      setClientData(resultAllClientData);
+    }
+  }, [fetchingAllClientRequest, resultAllClientRequest, activeButton]);
 
   return (
     <Page title="Partner Management">
       <Container>
         <ContentStyle>
-          {newJoinRequestData ? (
-            <TableAMCustom
-              data={newJoinRequestData}
-              headline="partner_management"
-              lengthRowsPerPage={10}
-            />
-          ) : (
+          {fetchingAllClientRequest && (
             <Skeleton variant="rectangular" sx={{ height: 250, borderRadius: 2 }} />
           )}
-          {/* <TableAMCustom
-            data={AM_PARTNER_MANAGEMENT}
-            headline="partner_management"
-            lengthRowsPerPage={10}
-          /> */}
+          {clientData && (
+            <>
+              {/* <Grid container spacing={3} alignItems="center" justifyContent="flex-start">
+                {CLIENT_STATUS.map((v, i) => (
+                  <Grid item key={i}>
+                    <Button
+                      variant="contained"
+                      onClick={() => {
+                        setActiveButton(v.label);
+                      }}
+                    >
+                      {v.label}
+                    </Button>
+                  </Grid>
+                ))}
+              </Grid> */}
+              <TableAMCustom
+                data={clientData}
+                headline="partner_management"
+                lengthRowsPerPage={10}
+              />
+            </>
+          )}
         </ContentStyle>
       </Container>
     </Page>
