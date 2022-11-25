68f26e8167174e9f6d0f185b88bf38fdd7a450a9
Hendy Irawan <ceefour666@gmail.com>
2022-11-25 08:10:02 +0000

    build(raise): prisma to use JSON logging

--- apps/raise/src/prisma/prisma.service.ts
+++ apps/raise/src/prisma/prisma.service.ts
@@ -1,17 +1,59 @@
 import { Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { ROOT_LOGGER } from 'src/libs/root-logger';
 
+/**
+ * See: https://docs.nestjs.com/recipes/prisma
+ */
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = ROOT_LOGGER.child({
+    'log.logger': 'PrismaService',
+    'event.category': 'database',
+    'event.module': 'postgresql',
+  });
+
   constructor(private configService: ConfigService) {
     super({
-      log: ['query', 'info', 'warn', 'error'],
+      // log is handled by listeners below which uses Winston
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'error' },
+        { emit: 'event', level: 'warn' },
+        { emit: 'event', level: 'info' },
+      ],
       datasources: {
         db: {
           url: configService.get('POSTGRES_URL'),
         },
       },
+    });
+    this.$on('error' as any, async (e: Prisma.LogEvent) => {
+      this.logger.info('dsddsd', configService.get('POSTGRES_URL'));
+      this.logger.error(e.message, {
+        '@timestamp': e.timestamp,
+        labels: { target: e.target },
+      });
+    });
+    this.$on('warn' as any, async (e: Prisma.LogEvent) => {
+      this.logger.warn(e.message, {
+        '@timestamp': e.timestamp,
+        labels: { target: e.target },
+      });
+    });
+    this.$on('info' as any, async (e: Prisma.LogEvent) => {
+      this.logger.info(e.message, {
+        '@timestamp': e.timestamp,
+        labels: { target: e.target },
+      });
+    });
+    this.$on('query' as any, async (e: Prisma.QueryEvent) => {
+      this.logger.debug(e.query, {
+        '@timestamp': e.timestamp,
+        labels: { params: JSON.stringify(e.params) },
+        'event.duration': e.duration * 1000000,
+      });
     });
   }
 
