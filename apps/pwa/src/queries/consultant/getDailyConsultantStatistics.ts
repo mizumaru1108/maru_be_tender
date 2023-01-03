dec86782440abf3791c586e6bd6cd4e41ab2aa95
Yayan Handani <others.handani@gmail.com>
2023-01-03 12:23:11 +0700

    fix: WIP change for daily status query

--- apps/pwa/src/queries/consultant/getDailyConsultantStatistics.ts
+++ apps/pwa/src/queries/consultant/getDailyConsultantStatistics.ts
@@ -1,15 +1,15 @@
 export const getDailyConsultantStatistics = `query getDailyConsultantStatistics($user_id: String = "", $first_date: timestamptz = "", $second_date: timestamptz = "") {
-  accepted_projects: proposal_log_aggregate(where: {reviewer_id: {_eq: $user_id}, _and: {action: {_eq: "accept"}, _and: {created_at: {_gte: $first_date}, _and: {created_at: {_lt: $second_date}}}}}) {
+  accepted_projects: proposal_log_aggregate(where: {reviewer_id: {_eq: $user_id}, _and: {action: {_eq: "accept"}, _and: {updated_at: {_gte: $first_date}, _and: {updated_at: {_lt: $second_date}}}}}) {
     aggregate {
       count
     }
   }
-  pending_projects: proposal_log_aggregate(where: {reviewer_id: {_eq: $user_id}, _and: {action: {_eq: "pending"}, , _and: {action: {_eq: "accept"}, _and: {created_at: {_gte: $first_date}, _and: {created_at: {_lt: $second_date}}}}}}) {
+  pending_projects: proposal_log_aggregate(where: {reviewer_id: {_eq: $user_id}, _and: {action: {_eq: "pending"}, , _and: {action: {_eq: "accept"}, _and: {updated_at: {_gte: $first_date}, _and: {updated_at: {_lt: $second_date}}}}}}) {
     aggregate {
       count
     }
   }
-  rejected_projects: proposal_log_aggregate(where: {reviewer_id: {_eq: $user_id}, _and: {action: {_eq: "rejected"}, _and: {_and: {created_at: {_gte: $first_date}, _and: {created_at: {_lt: $second_date}}}}}}) {
+  rejected_projects: proposal_log_aggregate(where: {reviewer_id: {_eq: $user_id}, _and: {action: {_eq: "rejected"}, _and: {_and: {updated_at: {_gte: $first_date}, _and: {updated_at: {_lt: $second_date}}}}}}) {
     aggregate {
       count
     }
