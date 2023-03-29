caec6a09f324ce51981accf639f6dc2c87785690
Yayan Handani <others.handani@gmail.com>
2023-03-29 22:31:27 +0700

    fix: track budgets in admin dashboard

--- apps/pwa/src/sections/admin/main/Main.tsx
+++ apps/pwa/src/sections/admin/main/Main.tsx
@@ -1,42 +1,14 @@
 import { Grid, Stack } from '@mui/material';
-import PathStats, { PathStatsProps } from './PathStats';
+import TrackBudget from 'sections/ceo/ceo-dashboard/TrackBudget';
 import Settings from './Settings';
 
-const data = [
-  {
-    title: 'ميزانية مسار المساجد',
-    totalBudget: '1.000.000',
-    spentBudget: '400.000',
-    reservedBudget: '400.000',
-  },
-  {
-    title: 'ميزانية مسار المنح الميسر',
-    totalBudget: '1.000.000',
-    spentBudget: '400.000',
-    reservedBudget: '400.000',
-  },
-  {
-    title: 'ميزانية مسار المبادرات',
-    totalBudget: '1.000.000',
-    spentBudget: '400.000',
-    reservedBudget: '400.000',
-  },
-  {
-    title: 'ميزانية مسار التعميدات',
-    totalBudget: '1.000.000',
-    spentBudget: '400.000',
-    reservedBudget: '400.000',
-  },
-] as PathStatsProps[];
 function Main() {
   return (
     <Stack direction="column" gap={5}>
-      <Grid container spacing={6}>
-        {data.map((item, index) => (
-          <Grid key={index} item md={6} xs={12}>
-            <PathStats {...item} />
-          </Grid>
-        ))}
+      <Grid container spacing={5}>
+        <Grid item md={12}>
+          <TrackBudget />
+        </Grid>
       </Grid>
       <Settings />
     </Stack>
