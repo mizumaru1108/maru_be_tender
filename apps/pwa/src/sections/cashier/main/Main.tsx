bf5cb0c3a92dd468b8dbd616e85f71bffd6b42a1
mizumaru1108 <haitsam.umar@gmail.com>
2023-05-25 14:04:00 +0700

    feat track budget in spv, pm, finance, cashier, fix bug arabic translation, fix bug project path layout position

--- apps/pwa/src/sections/cashier/main/Main.tsx
+++ apps/pwa/src/sections/cashier/main/Main.tsx
@@ -1,13 +1,31 @@
 import { Grid } from '@mui/material';
+import useAuth from 'hooks/useAuth';
+import useLocales from 'hooks/useLocales';
+import { getOneEmployee } from 'queries/admin/getAllTheEmployees';
+import TrackBudget from 'sections/cashier/main/TrackBudget';
+import { useQuery } from 'urql';
 import DailyStatistics from './DailyStatistics';
 import IncomingExchangePermissionRequests from './IncomingExchangePermissionRequests';
 import RequestsInProcess from './RequestsInProcess';
 
 function Main() {
+  const { translate } = useLocales();
+  const { user } = useAuth();
+
+  const [{ data, fetching, error }] = useQuery({
+    query: getOneEmployee,
+    variables: { id: user?.id },
+  });
+
+  if (fetching) return <>{translate('pages.common.loading')}</>;
+  if (error) return <>{error.message}</>;
   return (
     <Grid container spacing={4}>
       <Grid item md={12} xs={12}>
         <DailyStatistics />
+      </Grid>
+      <Grid item md={12}>
+        <TrackBudget path={data.data.employee_path} track_id={data.data.track_id} />
       </Grid>
       <Grid item md={12} xs={12}>
         <RequestsInProcess />
--- apps/pwa/src/sections/ceo/ceo-dashboard/index.tsx
+++ apps/pwa/src/sections/ceo/ceo-dashboard/index.tsx
@@ -1,21 +1,35 @@
 import { Grid } from '@mui/material';
 import DailyStatistics from './DailyStatistics';
-import TrackBudget from './TrackBudget';
 import DashboardProjectManagement from './ProjectManagement';
 import ProposalOnAmandementTable from '../../../components/table/ceo/proposal-on-amandement/ProposalOnAmandementTable';
 import { dispatch, useSelector } from 'redux/store';
 import { useEffect } from 'react';
 import { getTrackList } from 'redux/slices/proposal';
 import useAuth from 'hooks/useAuth';
+import TrackBudget from 'sections/ceo/ceo-dashboard/TrackBudget';
+import { useQuery } from 'urql';
+import { getOneEmployee } from 'queries/admin/getAllTheEmployees';
+import useLocales from 'hooks/useLocales';
 
 function CeoDashboard() {
+  const { translate } = useLocales();
   const { isLoading } = useSelector((state) => state.proposal);
   const activeRole = useAuth();
+  const { user } = useAuth();
   const role = activeRole!.activeRole;
+
+  const [{ data, fetching, error }] = useQuery({
+    query: getOneEmployee,
+    variables: { id: user?.id },
+  });
+
   useEffect(() => {
     dispatch(getTrackList(0, role as string));
   }, [activeRole, role]);
-  if (isLoading) return <>Loading</>;
+
+  if (fetching || isLoading) return <>{translate('pages.common.loading')}</>;
+  if (error) return <>{error.message}</>;
+
   return (
     <>
       <Grid container spacing={5}>
@@ -23,7 +37,7 @@ function CeoDashboard() {
           <DailyStatistics />
         </Grid>
         {/* <Grid item md={12}>
-          <TrackBudget />
+          <TrackBudget track_id={data.data.track_id} />
         </Grid> */}
         <Grid item md={12}>
           <DashboardProjectManagement />
