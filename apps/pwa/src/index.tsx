83c7972398e123c708854cfc5c4551e954da0bec
Hendy Irawan <ceefour666@gmail.com>
2023-06-15 08:58:05 +0700

    build: Datadog RUM

--- apps/pwa/src/index.tsx
+++ apps/pwa/src/index.tsx
@@ -17,27 +17,29 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import 'react-quill/dist/quill.snow.css';
 
 // slick-carousel
-import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 // lazy image
+import 'react-lazy-load-image-component/src/effects/black-and-white.css';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import 'react-lazy-load-image-component/src/effects/opacity.css';
-import 'react-lazy-load-image-component/src/effects/black-and-white.css';
 
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider as ReduxProvider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 // @mui
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 // redux
-import { store, persistor } from './redux/store';
+import { persistor, store } from './redux/store';
 // contexts
-import { SettingsProvider } from './contexts/SettingsContext';
 import { CollapseDrawerProvider } from './contexts/CollapseDrawerContext';
+import { SettingsProvider } from './contexts/SettingsContext';
+
+// Datadog RUM
+import './utils/datadog-rum';
 
 // Check our docs
 // https://docs-minimals.vercel.app/authentication/ts-version
@@ -48,17 +50,16 @@ import { AuthProvider } from './contexts/JWTContext';
 // import { AuthProvider } from './contexts/AwsCognitoContext';
 
 //
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 import DateFnsUtils from '@date-io/date-fns';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // ----------------------------------------------------------------------
 
-import { ar } from 'date-fns/locale';
-import { Provider } from 'urql';
 import CreateClient from 'CreateClient';
+import { ar } from 'date-fns/locale';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
