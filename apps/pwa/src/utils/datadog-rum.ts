c3339a316d3bb222b714d4e1522e2072b4ebe620
Hendy Irawan <ceefour666@gmail.com>
2023-06-15 17:58:40 +0700

    build: Datadog RUM

--- apps/pwa/src/utils/datadog-rum.ts
+++ apps/pwa/src/utils/datadog-rum.ts
@@ -3,7 +3,12 @@ import { HASURA_GRAPHQL_URL, TMRA_RAISE_URL } from '../config';
 
 console.debug('Datadog RUM:', process.env.REACT_APP_DATADOG_RUM ? true : false);
 if (process.env.REACT_APP_DATADOG_RUM) {
-  console.debug('Datadog RUM:', process.env.REACT_APP_DATADOG_RUM ? true : false);
+  if (!process.env.REACT_APP_SERVICE_ENV) {
+    throw new Error('REACT_APP_SERVICE_ENV is required');
+  }
+  if (!process.env.REACT_APP_SERVICE_VERSION) {
+    throw new Error('REACT_APP_SERVICE_VERSION is required');
+  }
   datadogRum.init({
     applicationId: 'e38d9e0d-a6cc-4a8e-aed4-073ab605d9ec',
     clientToken: 'pub06d25281552d9cf7a33398db806b8b99',
