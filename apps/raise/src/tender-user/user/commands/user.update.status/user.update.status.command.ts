54d3814fca2c30352aaad81aab5fa4bdef069690
rdanang-dev <rdanang.dev@gmail.com>
2023-09-24 09:36:28 +0700

    feat: implement new hbs for email notif after acc by account manager

--- apps/raise/src/tender-user/user/commands/user.update.status/user.update.status.command.ts
+++ apps/raise/src/tender-user/user/commands/user.update.status/user.update.status.command.ts
@@ -8,6 +8,8 @@ import { UserEntity } from '../../entities/user.entity';
 import { TenderUserStatusLogRepository } from '../../repositories/tender-user-status-log.repository';
 import { TenderUserRepository } from '../../repositories/tender-user.repository';
 import { UserStatusEnum } from '../../types/user_status';
+import { ISendNotificaitonEvent } from '../../../../proposal-management/proposal/entities/proposal.entity';
+import { EmailService } from '../../../../libs/email/email.service';
 export class UserUpdateStatusCommand {
   acc_manager_id: string;
   request: UserStatusUpdateDto;
@@ -27,6 +29,7 @@ export class UserUpdateStatusCommandHandler
 {
   constructor(
     private readonly prismaService: PrismaService,
+    private readonly emailService: EmailService,
     private readonly userRepo: TenderUserRepository,
     private readonly statusLogRepo: TenderUserStatusLogRepository,
   ) {}
@@ -87,114 +90,43 @@ export class UserUpdateStatusCommandHandler
         },
       );
 
-      //   await this.sendChangeStatusNotification(
-      //     response.user_status_log,
-      //     request.selectLang,
-      //   );
-      //   return response;
-      // }
+      const notifPayloads: ISendNotificaitonEvent[] = [];
 
-      // async sendChangeStatusNotification(
-      //   status_log: IUserStatusLogResponseDto['data'],
-      //   selected_language?: 'ar' | 'en',
-      // ) {
-      //   let subject = '';
-      //   let clientContent = '';
-      //   const employeeContent = `You have changed the account status of ${status_log.user_detail.email} to ${status_log.user_status.id}`;
-      //   if (status_log.user_status.id === UserStatusEnum.ACTIVE_ACCOUNT) {
-      //     subject = 'Your account has been activated!';
-      //     clientContent = `Your account (${status_log.user_detail.email}) has been activated by the Account Manager`;
-      //   } else if (
-      //     status_log.user_status.id === UserStatusEnum.WAITING_FOR_ACTIVATION
-      //   ) {
-      //     subject = "You're account is waiting for activation!";
-      //     clientContent =
-      //       'You have been successfully registered as a user on tender-app, please be patience untill your account being reviewed and approved!';
-      //   } else if (status_log.user_status.id === UserStatusEnum.SUSPENDED_ACCOUNT) {
-      //     subject = 'Your account has been suspended!';
-      //     clientContent = `Your account (${status_log.user_detail.email}) has been suspended by the Account Manager`;
-      //   } else if (status_log.user_status.id === UserStatusEnum.CANCELED_ACCOUNT) {
-      //     subject = 'Your account has been canceled!';
-      //     clientContent = `Your account (${status_log.user_detail.email}) has been canceled by the Account Manager`;
-      //   } else if (status_log.user_status.id === UserStatusEnum.REVISED_ACCOUNT) {
-      //     subject = 'Your account has been revised!';
-      //     clientContent = `Your account (${status_log.user_detail.email}) has been revised by the Account Manager`;
-      //   } else if (
-      //     status_log.user_status.id === UserStatusEnum.WAITING_FOR_EDITING_APPROVAL
-      //   ) {
-      //     subject = 'Your account editing request has been sent!';
-      //     clientContent = `Your account editing request has been sent to the Account Manager, please be patience untill your account being reviewed and approved!`;
-      //   }
+      if (request.status === UserStatusEnum.ACTIVE_ACCOUNT) {
+        notifPayloads.push({
+          notif_type: 'EMAIL',
+          user_id: dbRes.updated_user.id,
+          user_email: dbRes.updated_user.email,
+          subject: '',
+          content: '',
+          email_type: 'template',
+          emailTemplateContext: {
+            name: dbRes.updated_user.employee_name,
+            client_email: dbRes.updated_user.email,
+          },
+          emailTemplatePath: `tender/ar/account/account_activate_by_acc_manager`,
+        });
+      }
 
-      //   // email notification
-      //   const clientEmailNotifPayload: SendEmailDto = {
-      //     mailType: 'plain',
-      //     to: status_log.user_detail.email,
-      //     from: 'no-reply@hcharity.org',
-      //     subject,
-      //     content: clientContent,
-      //   };
+      if (notifPayloads && notifPayloads.length > 0) {
+        for (const notifPayload of notifPayloads) {
+          if (
+            notifPayload.notif_type === 'EMAIL' &&
+            notifPayload.user_email &&
+            notifPayload.email_type
+          ) {
+            this.emailService.sendMail({
+              mailType: notifPayload.email_type,
+              to: notifPayload.user_email,
+              subject: notifPayload.subject,
+              content: notifPayload.content,
+              templateContext: notifPayload.emailTemplateContext,
+              templatePath: notifPayload.emailTemplatePath,
+            });
+          }
+        }
+      }
 
-      //   if (status_log.user_status.id === UserStatusEnum.SUSPENDED_ACCOUNT) {
-      //     clientEmailNotifPayload.mailType = 'template';
-      //     clientEmailNotifPayload.templatePath = `tender/${
-      //       selected_language || 'ar'
-      //     }/account/account_deactivation`;
-      //     clientEmailNotifPayload.templateContext = {
-      //       name: status_log.user_detail.employee_name || 'Client...',
-      //     };
-      //   }
-      //   this.emailService.sendMail(clientEmailNotifPayload);
-
-      //   const clientWebNotifPayload: CreateNotificationDto = {
-      //     type: 'ACCOUNT',
-      //     specific_type: 'NEW_SUSPENDED_ACCOUNT',
-      //     user_id: status_log.user_detail.id,
-      //     subject,
-      //     content: clientContent,
-      //   };
-      //   this.tenderNotificationService.create(clientWebNotifPayload);
-
-      //   const clientPhone = isExistAndValidPhone(
-      //     status_log.user_detail.mobile_number,
-      //   );
-      //   if (clientPhone) {
-      //     this.twilioService.sendSMS({
-      //       to: clientPhone,
-      //       body: subject + ',' + clientContent,
-      //     });
-      //   }
-
-      //   if (status_log.account_manager_detail) {
-      //     const employeeEmailNotifPayload: SendEmailDto = {
-      //       mailType: 'plain',
-      //       to: status_log.account_manager_detail.email,
-      //       from: 'no-reply@hcharity.org',
-      //       subject,
-      //       content: employeeContent,
-      //     };
-      //     this.emailService.sendMail(employeeEmailNotifPayload);
-
-      //     // const employeeWebNotifPayload: CreateNotificationDto = {
-      //     //   type: 'ACCOUNT',
-      //     //   specific_type: 'NEW_SUSPENDED_ACCOUNT',
-      //     //   user_id: status_log.account_manager_detail.id,
-      //     //   subject,
-      //     //   content: employeeContent,
-      //     // };
-      //     // this.tenderNotificationService.create(employeeWebNotifPayload);
-
-      //     const accManagerPhone = isExistAndValidPhone(
-      //       status_log.account_manager_detail.mobile_number,
-      //     );
-      //     if (accManagerPhone) {
-      //       this.twilioService.sendSMS({
-      //         to: accManagerPhone,
-      //         body: subject + ',' + employeeContent,
-      //       });
-      //     }
-      //   }
-      // }
       return {
         updated_user: dbRes.updated_user,
         created_status_log: dbRes.created_status_log,
