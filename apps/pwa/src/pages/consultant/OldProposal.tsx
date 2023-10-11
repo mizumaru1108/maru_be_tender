8c4ee3c769d35bf3c464cccd24214f308efe1cf7
mizumaru1108 <haitsam.umar@gmail.com>
2023-10-11 15:27:27 +0700

    fix: refactor table old proposal for all role

--- apps/pwa/src/pages/consultant/OldProposal.tsx
+++ apps/pwa/src/pages/consultant/OldProposal.tsx
@@ -3,9 +3,16 @@ import Page from 'components/Page';
 import { styled } from '@mui/material/styles';
 import useLocales from '../../hooks/useLocales';
 import OldProposalTable from '../../components/table/old-proposal/OldProposalTable';
+import useAuth from 'hooks/useAuth';
+import { dispatch, useSelector } from 'redux/store';
+import React from 'react';
+import { getTrackList } from 'redux/slices/proposal';
 
 function OldProposalPage() {
   const { translate } = useLocales();
+  const { activeRole } = useAuth();
+  const { loadingProps } = useSelector((state) => state.proposal);
+
   const ContentStyle = styled('div')(({ theme }) => ({
     maxWidth: '100%',
     minHeight: '100vh',
@@ -14,6 +21,12 @@ function OldProposalPage() {
     flexDirection: 'column',
     gap: 20,
   }));
+
+  React.useEffect(() => {
+    dispatch(getTrackList(0, activeRole! as string));
+  }, [activeRole]);
+
+  if (loadingProps.laodingTrack) return <>{translate('pages.common.loading')}</>;
 
   return (
     <Page title={translate('pages.common.old_proposal')}>
--- apps/pwa/src/pages/project-supervisor/OldProposal.tsx
+++ apps/pwa/src/pages/project-supervisor/OldProposal.tsx
@@ -3,9 +3,15 @@ import Page from 'components/Page';
 import { styled } from '@mui/material/styles';
 import useLocales from '../../hooks/useLocales';
 import OldProposalTable from '../../components/table/old-proposal/OldProposalTable';
+import useAuth from 'hooks/useAuth';
+import { dispatch, useSelector } from 'redux/store';
+import React from 'react';
+import { getTrackList } from 'redux/slices/proposal';
 
 function OldProposalPage() {
   const { translate } = useLocales();
+  const { activeRole } = useAuth();
+  const { loadingProps } = useSelector((state) => state.proposal);
   const ContentStyle = styled('div')(({ theme }) => ({
     maxWidth: '100%',
     minHeight: '100vh',
@@ -14,6 +20,11 @@ function OldProposalPage() {
     flexDirection: 'column',
     gap: 20,
   }));
+  React.useEffect(() => {
+    dispatch(getTrackList(0, activeRole! as string));
+  }, [activeRole]);
+
+  if (loadingProps.laodingTrack) return <>{translate('pages.common.loading')}</>;
 
   return (
     <Page title={translate('pages.common.old_proposal')}>
